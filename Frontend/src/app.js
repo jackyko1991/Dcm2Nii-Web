fa061b9480f2d06612326fca361a875236d4a6c9
Jacky Ko <jackkykokoko@gmail.com>
2018-11-03 17:53:58 +0800

    dependency update

--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -1,18 +1,48 @@
 import React from 'react';
-import {render} from 'react-dom';
+import HomePage from './components/HomePage';
+// import AboutPage from '../components/AboutPage';
+// import ContactPage from '../components/ContactPage';
+// import FAQPage from '../components/FAQPage';
+// import NotFoundPage from '../components/NotFoundPage';
+// import ServicesPage from '../components/ServicesPage';
+import HeaderAppBar from './components/Header';
+import { withStyles } from '@material-ui/core/styles';
 import {MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-// import HeaderAppBar from './components/Header';
-import AppRouter from './routers/AppRouter';
-// import './styles/styles.css'
-;
 
 const theme = createMuiTheme({
 });
 
+const styles = theme=> ({
+    root:{
+    },
+})
+
 const App = () => (
 	<MuiThemeProvider theme={theme}>
-		<AppRouter />
+		<div>
+			<HeaderAppBar />
+			<HomePage />
+		</div>
 	</MuiThemeProvider>
 );
+export default withStyles(styles)(App);
 
-render(<App />, document.getElementById('app'));
+
+// import HeaderAppBar from './components/Header';
+// import AppRouter from './routers/AppRouter';
+// // import './styles/styles.css'
+// ;
+
+
+
+// const App = () => (
+
+// );
+
+
+	
+	// <Route path="/about" component={AboutPage} />
+	// <Route path="/contact" component={ContactPage} />
+	// <Route path="/FAQ" component={FAQPage} />
+	// <Route path="/services" component={ServicesPage} />
+	// <Route component={NotFoundPage} />
